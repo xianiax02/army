039dfc2bd2aa3491c9d6474cf0c8589b43eae127
xianiax02 <wuchialing0825@gmail.com>
2024-07-04 20:35:56 +0900

    33problem

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
-a=ord(input())
-print(a)
+a=ord((input()))
+print(chr(a+1))
 #print(a,b,sep=':')
 #파이썬은 print("%x"%변수명)
 #입력 진수 설정 int(a,16)
 #유니코드 값으로 변환 ord(input())
+#단항연산자 -
+#유니코드 값 출력 chr(유니코드)
