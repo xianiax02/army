bedb3d5b3942be0534d01eb0979646def2687a5f
xianiax02 <wuchialing0825@gmail.com>
2024-07-04 22:40:55 +0900

    49problem

--- index.py
+++ index.py
@@ -1,8 +1,14 @@
-a=ord((input()))
-print(chr(a+1))
+a,b=input().split(" ")
+if (int(a)==int(b)):
+    print('True')
+else:
+    print('False')
 #print(a,b,sep=':')
-#파이썬은 print("%x"%변수명)
+#파이썬은 print("%x"%변수명), %.2f ->소숫점 이하 두번쨰 자리까지의 정확도
 #입력 진수 설정 int(a,16)
 #유니코드 값으로 변환 ord(input())
 #단항연산자 -
 #유니코드 값 출력 chr(유니코드)
+#거듭제곱 연산자 **, 몫 연산 //, 나머지 %
+#비트시프트 연산 n<<1 n을 이진법 상 한자리 수 좌로 옮긴것
+#논리 결과는 True or False 로 산출됨
