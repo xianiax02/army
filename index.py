3923c46e3b30c7ff959f94b12b41ad392e6ed657
xianiax02 <wuchialing0825@gmail.com>
2024-07-04 23:14:21 +0900

    60problems

--- index.py
+++ index.py
@@ -1,8 +1,6 @@
 a,b=input().split(" ")
-if (int(a)==int(b)):
-    print('True')
-else:
-    print('False')
+a=int(a)&int(b)
+print(a)
 #print(a,b,sep=':')
 #파이썬은 print("%x"%변수명), %.2f ->소숫점 이하 두번쨰 자리까지의 정확도
 #입력 진수 설정 int(a,16)
@@ -12,3 +10,5 @@ else:
 #거듭제곱 연산자 **, 몫 연산 //, 나머지 %
 #비트시프트 연산 n<<1 n을 이진법 상 한자리 수 좌로 옮긴것
 #논리 결과는 True or False 로 산출됨
+#boolean 예약어: not, and , or
+#비트단위 연산자 not:~, and:&,or:|,xor:^,leftshift:<<,rightshift:>>
