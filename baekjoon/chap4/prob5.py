98931e379c58dd5f2f350548b6d70af2a88ceebe
xianiax02 <wuchialing0825@gmail.com>
2024-07-15 23:37:56 +0900

    prob5

--- baekjoon/chap4/prob5.py
+++ baekjoon/chap4/prob5.py
@@ -1,12 +1,21 @@
 import sys
-a=[]
-biggest=0
-for i in range(9):
-    new=int(sys.stdin.readline())
-    a.append(new)
-    biggest=max(new,biggest) #기존의 최댓값과 비교해야지 연속된 두 값을 비교해서 되지 않음!!
+n,m=sys.stdin.readline().split(' ')
+n=int(n)
+m=int(m)
+baskets=[]
+for i in range(n):
+    baskets.append(0)
+for i in range(m):
+    a,b,c=sys.stdin.readline().split(' ')
+    a=int(a)
+    b=int(b)
+    c=int(c)
+    for i in range(a,b+1): #여기는 2에서 5번까지라하면 닫힌 구간임을 고려하지 못
+        baskets[i-1]=c
+#    for i in range(len(baskets)):
+ #       print(baskets[i],end=' ')
+  #  print()
+for i in range(len(baskets)):
+    print(baskets[i],end=' ')
+    
 
-print(biggest)
-print(a.index(biggest)+1)
-
-#사실 그냥 max(a)하면 최댓값 나옴 
