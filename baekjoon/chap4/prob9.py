b1a56fbba3068753defe1953b3ad7a8c83399276
xianiax02 <wuchialing0825@gmail.com>
2024-07-18 20:42:30 +0900

    second try

--- baekjoon/chap4/prob9.py
+++ baekjoon/chap4/prob9.py
@@ -10,6 +10,17 @@ for a in range(n):
     i,j=list(map(int,sys.stdin.readline().split(' ')))
     change=baskets[i+1:j+2]
     change.reverse()
+    for k in range(i-j+1):
+        baskets[k]=change[k]
+    print()
+    print(a,'time',end='')
+    for k in range(m):
+        print(baskets[k],end=' ')
+    print('change')
+    print(a,'time',end='')
+    for k in range(m):
+        print(baskets[k],end=' ')
+        
     
 for i in range(m):
     print(baskets[i],end=' ')
