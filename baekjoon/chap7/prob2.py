cc70506f869a1f63cb19ff52caadc23ff9b0a8e4
xianiax02 <wuchialing0825@gmail.com>
2024-08-21 16:09:06 +0900

    xhap7 chap7

--- baekjoon/chap7/prob2.py
+++ baekjoon/chap7/prob2.py
@@ -1,39 +1,49 @@
-#MATRIX ADDITION
-
-def createmat(m,n):
-    mat=[]
+#matrix addition
+#mn행렬은 list[m][n]의 2차원 행렬임
+def createmat(m): #입력할 행렬의 행 수를 받으면 해당 행 만큼의 행렬을 입력받음
+    matrix=[]
     for i in range(m):
+        matrix append(list(map(int,input().split(' '))))
         #row=[]
         #for i in range(n):
-            #row.append(input().split())
-        #row.append(input().split())
-        mat.append(list(map(int,input().split())))
-    return mat
+        #row.append(input().split(' '))
+        #matrix.append(row)
 
+    return matrix
 
-def addmat(mat1,mat2):
+def addmat(matrix1,matrix2):
     result=[]
-    for i in range(len(mat1)): #같은 열만큼 행을 더해야함
-        resultrow=[]
-        for j in range(len(mat1[i])):
-            resultrow.append(mat1[i][j]+mat2[i][j])
-        result.append(resultrow)
+    for i in range(len(matrix1)): #각 행마다
+        row=[]
+        for j in range(len(matrix1)):
+            row.append(matrix1[i][j]+matrix2[i][j])
+        result.append(row)
     return result
 
 def printmat(mat):
     for i in range(len(mat)):
-        for j in range(len(mat[i])):
-            print(mat[i][j],end=' ')
-        print('\n',end='')
+        for j in mat[i]:
+            print(j,end=' ')
+        print('',end='\n')
+    return
+
+
+#99행렬의 최댓값과 그 인덱스를 구하는 방법
+#1.모두 리스트에 넣어서 max()적용
+#2. maxlist를 만들어서 그 maxlist에서 최댓값 추출(maxlist는 행과 열까지 반환)
+#max는 iterable object의 길이, 또는 첫 값을 기준으로 비교함에 유의;
 
 def findmax(mat):
-    maxnum=0
-    for i in mat:
-        maxnum=max(maxnum,max(i))
-    return maxnum
-
-mat1=createmat()
-
-    
-
-        
+    maxlist=[]
+    for i in range(len(mat)):
+        box=[max(mat[i],i)]
+        maxlist.append(box)
+    maxnum, m=max(maxlist)
+    n=mat[m].index(maxnum)
+    m+=1
+    n+=1
+    return maxnum,m,n
+mat=createmat(9)
+maxnum,m,n=findmax(mat)
+print(maxnum)
+print(m,n)
